8645dd3eaa80d75add525d4a570c17aaeedce0f5
JacobTam <jatam@tesla.com>
2020-06-02 11:11:49 -0600

    Ballon Time

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -1,11 +1,7 @@
-/**
- * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
- */
 
 // The editor creator to use.
-import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-import { InsertImage } from './customPlugin/InsertImage';
+
+import BalloonEditorBase from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor';
 import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters';
 import SpecialCharactersEssentials from '@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
@@ -18,36 +14,28 @@ import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-import Table from '@ckeditor/ckeditor5-table/src/table';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
-export default class ClassicEditor extends ClassicEditorBase {}
+export default class BalloonEditor extends BalloonEditorBase {}
 
-ClassicEditor.builtinPlugins = [
+BalloonEditor.builtinPlugins = [
 	Essentials,
 	Autoformat,
 	Bold,
 	Italic,
 	BlockQuote,
-	CKFinder,
 	Heading,
 	Indent,
 	Link,
 	List,
-	MediaEmbed,
 	Paragraph,
 	PasteFromOffice,
-	Table,
-	TableToolbar,
 	TextTransformation,
 	Alignment,
 	Underline,
@@ -56,12 +44,11 @@ ClassicEditor.builtinPlugins = [
 	Superscript,
 	HorizontalLine,
 	SpecialCharacters,
-	SpecialCharactersEssentials,
-	InsertImage
+	SpecialCharactersEssentials
 ];
 
 // Editor configuration.
-ClassicEditor.defaultConfig = {
+BalloonEditor.defaultConfig = {
 	toolbar: {
 		items: [
 			'heading',
@@ -81,20 +68,10 @@ ClassicEditor.defaultConfig = {
 			'outdent',
 			'horizontalline',
 			'|',
-			'insertImage',
 			'blockQuote',
-			'insertTable',
-			'mediaEmbed',
 			'specialCharacters',
 			'undo',
 			'redo'
-		]
-	},
-	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
 		]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
